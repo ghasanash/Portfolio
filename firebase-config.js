f1bdd18000eb524c3d2e3eb72d6b0b73e0b9387f
ghasanash <116428897+ghasanash@users.noreply.github.com>
2025-06-02 13:38:27 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAJQ7NWKaDuJnJ_fBDeyrllv3RmzsRCm0k",
-  authDomain: "ghasan-portfolio.firebaseapp.com",
-  databaseURL: "https://ghasan-portfolio-default-rtdb.firebaseio.com",
-  projectId: "ghasan-portfolio",
-  storageBucket: "ghasan-portfolio.firebasestorage.app",
-  messagingSenderId: "276513302346",
-  appId: "1:276513302346:web:70a46e0c9951a6f7bec3b3",
-  measurementId: "G-HX0LXFM3LX"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
